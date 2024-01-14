9badf66542dfe9dd40b26deaa197e2cff94c40f1
kohtet0 <thwinlinhtetthwinlinhtet@gmail.com>
2024-01-14 13:43:43 +0630

    ts day2 not finish to tsc edit

---
+++ 10-TypeScript/TS-DAY2/index.ts
@@ -0,0 +1,55 @@
+// const arr: (string | number | true)[] = ["ko htet", 22, true];
+
+// type Person = {
+//   name: string;
+//   age: number;
+//   gender: "male" | "female";
+// };
+
+// const objArr: Person[] = [
+//   { name: "kohtet", age: 22, gender: "male" },
+//   { name: "Mya Mya", age: 19, gender: "female" },
+// ];
+
+// console.log(objArr[1].gender);
+
+// let ayer: any = 1;
+
+// ayer = "hello world!";
+// ayer = true;
+// ayer = ["hello", 2];
+// ayer = { name: "ko htet", age: 22 };
+// ayer = undefined;
+// ayer = null;
+
+// console.log(ayer);
+
+// enum Gender {
+//     male,
+//     female,
+//     other
+// }
+
+// type Person = {
+//     name : string,
+//     age: number,
+//     gender : Gender
+// }
+
+// const person: Person = {
+//     name: "ko htet",
+//     age: 22,
+//     gender: Gender.male
+// }
+
+// if (person.gender === Gender.male) {
+//     console.log("u are male");
+// }
+
+// const sum = (x: number, y:number): number => {
+//     return x + y;
+// }
+
+// const calc = sum;
+
+// console.log(calc(1, 2));
