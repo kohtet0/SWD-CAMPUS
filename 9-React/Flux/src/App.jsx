fda4564ff6c4f1314bef8e0a8c5dc94da32d61d5
kohtet0 <thwinlinhtetthwinlinhtet@gmail.com>
2024-01-30 11:07:43 +0630

    flux pattern

--- 9-React/Flux/src/App.jsx
+++ 9-React/Flux/src/App.jsx
@@ -2,43 +2,36 @@ import React, { useReducer } from "react";
 import { Button } from "./components/ui/button";
 
 const reducer = (store, action) => {
-  switch (action.type) {
-    case "increment":
-      {
-        return (store = store + 1);
-      }
-      break;
-    case "decrement":
-      {
-        if (store > 0) {
-          return (store = store - 1);
-        } else {
-          return store;
-        }
-      }
-      break;
-    default: {
+  const condition = action.type;
+  if (condition === "increment") {
+    return (store += 1);
+  } else if (condition === "decrease") {
+    if (store > 0) {
+      return (store -= 1);
+    } else {
       return store;
     }
+  } else {
+    return store;
   }
 };
 
 const App = () => {
-  const [count, dispatch] = useReducer(reducer, 0);
+  const [state, dispatcher] = useReducer(reducer, 0);
 
-  const handleRemove = () => {
-    dispatch({ type: "decrement" });
+  const handleAddBtn = () => {
+    dispatcher({ type: "increment" });
   };
 
-  const handleAdd = () => {
-    dispatch({ type: "increment" });
+  const handleRemoveBtn = () => {
+    dispatcher({ type: "decrease" });
   };
 
   return (
     <div className="container">
       <h1 className=" text-2xl font-bold mb-5">Add & Remove</h1>
       <div className="flex items-center gap-5">
-        <Button onClick={handleRemove}>
+        <Button onClick={handleRemoveBtn}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -50,8 +43,8 @@ const App = () => {
             <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
           </svg>
         </Button>
-        <h2 className=" text-xl">{count}</h2>
-        <Button onClick={handleAdd}>
+        <h2 className=" text-xl">{state}</h2>
+        <Button onClick={handleAddBtn}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
